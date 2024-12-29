d0379e32e2e0f0101a686ca35b0ae37b08d158fc
Ayush Bhandari <166861304+AyushZorix@users.noreply.github.com>
2024-12-29 19:55:55 +0530

    Create Rolling Hash.cpp

---
+++ ayush/Rolling Hash.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define d 256
+#define q 101
+
+void rollingHash(string text, string pattern) {
+    int M = pattern.length();
+    int N = text.length();
+    int i, j;
+    int p = 0;
+    int t = 0;
+    int h = 1;
+
+    for (i = 0; i < M - 1; i++)
+        h = (h * d) % q;
+
+    for (i = 0; i < M; i++) {
+        p = (p * d + pattern[i]) % q;
+        t = (t * d + text[i]) % q;
+    }
+
+    for (i = 0; i <= N - M; i++) {
+        if (p == t) {
+            for (j = 0; j < M; j++) {
+                if (text[i + j] != pattern[j])
+                    break;
+            }
+            if (j == M) {
+                cout << "Pattern found at index " << i << endl;
+            }
+        }
+
+        if (i < N - M) {
+            t = (d * (t - text[i] * h) + text[i + M]) % q;
+
+            if (t < 0)
+                t = (t + q);
+        }
+    }
+}
+
+int main() {
+    string text;
+    string pattern;
+    cin>>text>>pattern;
+    cout<<text<<pattern<<endl;
+    rollingHash(text, pattern);
+    return 0;
+}
